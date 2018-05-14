a0c1ddc6d7f656bc0051aca46dc6f8bd90d5a433
Tapan Chugh <t-tachug@microsoft.com>
2018-05-14 18:54:37 +0000

    Minor bug fix

--- custom/setup-c.el
+++ custom/setup-c.el
@@ -21,7 +21,7 @@
 ;; “python”: What Python developers use for extension modules
 ;; “java”: The default style for java-mode (see below)
 ;; “user”: When you want to define your own style
-(setq c-default-style) "stroustrup" ;; set style to "linux"
+(setq c-default-style "stroustrup") ;; set style to "linux"
 
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode) ;
